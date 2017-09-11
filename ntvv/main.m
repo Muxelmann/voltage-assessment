99916a8c006a6054951119d318f6b89f87adf94b
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-09-11 09:42:02 +0100

    All CIM-exports are now converted

---
+++ ntvv/main.m
@@ -0,0 +1,14 @@
+close all
+clear
+clc
+
+all_cim_exports = dir(fullfile('CIM Export 31_03_2017 - confidential', 'CIM_Export*'));
+root_output_dir = 'DSS Export - confidential';
+cim_equipment = 'Equipment DB - confidential';
+
+for i = 1:length(all_cim_exports)
+    cim_export = fullfile(all_cim_exports(i).folder, all_cim_exports(i).name);
+    output_dir = fullfile(root_output_dir, strrep(all_cim_exports(i).name, 'CIM_', 'DSS_'));
+
+    cim_converter(cim_export, cim_equipment, output_dir);
+end
