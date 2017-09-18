9e57e952aa0f1aeb09c889a8b0935e436d667744
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-09-18 16:21:58 +0100

    Adding coordinates to element now (missing loads)

--- ntvv/python/cimConverter/CIMClass.py
+++ ntvv/python/cimConverter/CIMClass.py
@@ -1,6 +1,8 @@
 import os
 import xml.etree.ElementTree as et
 import csv
+import re
+import math
 
 from cimConverter import logger
 
@@ -8,18 +10,18 @@ from cimConverter import logger
 class CIMClass:
 	"""CIMClass contains all functionality to convert CIM to OpenDSS"""
 
-	_ele = []
+	_ele = list()
 	_output_dir = None
-	_equipment = {}
-	_dss_ele = []
-	_ele_buffers = []
+	_equipment = dict()
+	_dss_ele = dict()
+	_ele_buffers = list()
 
 	def __init__(self, output_dir):
 		"""Initialises the CIMClass with logging enabled"""
-		self._ele = []
+		self._ele = list()
 		self._output_dir = None
-		self._equipment = {}
-		self._dss_ele = []
+		self._equipment = dict()
+		self._dss_ele = dict()
 
 		if not os.path.exists(output_dir):
 			os.makedirs(output_dir)
@@ -53,14 +55,60 @@ class CIMClass:
 			new_ele = self.get_cim_element(rdf_child)
 			if new_ele is not None:
 				if new_ele['id'] in [ids['id'] for ids in self._ele]:
-					logger.warning('an element ({}) was already imported'.format(new_ele['id']))
+					logger.debug('an element ({}) was already imported'.format(new_ele['id']))
 					continue
 				self._ele.append(new_ele)
 
 	def add_all_coordinates(self, gml_path):
 		"""Adds coordinates to the corresponding CIM element"""
-		# TODO: Add coordinates to CIM element
-		pass
+		if not os.path.exists(gml_path):
+			logger.warning('could not find GML file: {}'.format(gml_path))
+			return
+
+		xml_data = et.parse(gml_path)
+		nmm_root = xml_data.getroot()
+
+		if nmm_root.tag.split('}')[-1] != 'NMMFeatureCollection':
+			logger.warning('no NMMFeatureCollection found for file: {}'.format(gml_path))
+			return
+
+		for nmm_data in nmm_root:
+			if nmm_data.tag.split('}')[-1] != 'DeviceMember':
+				logger.warning('found unidentified NMM element ({}) in: {}'.format(nmm_data, gml_path))
+				continue
+
+			nmm_dev = self.get_nmm_device(nmm_data)
+			cim_ele = self.get_element_by_id(nmm_dev['id'])
+			if cim_ele is None:
+				continue
+			self._ele.remove(cim_ele)
+			cim_ele['coords'] = [float(c) for c in nmm_dev['coords']]
+			self._ele.append(cim_ele)
+
+	@staticmethod
+	def get_nmm_device(nmm_data):
+		if nmm_data[0].tag.split('}')[-1] != 'Device':
+			logger.warning('nmm data for {} does not contain a nmm:Device in'.format(nmm_data))
+			return
+
+		nmm_data = nmm_data[0]
+		nmm_dev = dict()
+		for nmm_dev_data in nmm_data:
+			tag = nmm_dev_data.tag.split('}')[-1]
+			if tag == 'ID':
+				nmm_dev['id'] = nmm_dev_data.text
+			elif tag == 'NMMGeometry':
+				coord_type = None
+				for coord_data in nmm_dev_data:
+					coord_tag = coord_data.tag.split('}')[-1]
+					if coord_tag == 'FieldName':
+						coord_type = coord_data.text
+					elif coord_type is not None:
+						if coord_tag in ['LineString', 'Point']:
+							nmm_dev['coords'] = coord_data[0].text.split(' ')
+						else:
+							logger('no idea how to read coordinates for {}'.format(nmm_data))
+		return nmm_dev
 
 	def add_equipment(self, equ_path):
 		"""Adds equipment informantion like it is stored in CIM DB"""
@@ -70,15 +118,18 @@ class CIMClass:
 			return
 
 		_, equ_file_name = os.path.split(equ_path)
-		equ_name = equ_file_name.split('.')[0]
+		equ_name, suffix = equ_file_name.split('.')
+		if suffix.lower() != 'csv':
+			logger.warning('equipment file is not a CSV file: {}'.format(equ_file_name))
+			return
 
 		with open(equ_path, newline='') as equ_file:
 			csv_reader = csv.reader(equ_file, delimiter=',')
 			header = next(csv_reader)
 			# Define the database
-			csv_data = {}
+			csv_data = dict()
 			for data in csv_reader:
-				csv_data[data[0]] = {}
+				csv_data[data[0]] = dict()
 				for i in range(1, len(data)):
 					if len(data[i]) == 0:
 						continue
@@ -97,7 +148,7 @@ class CIMClass:
 		"""Analyses and makes a dictionary containing the CIM element data"""
 		logger.debug('analysing: {}'.format(rdf_data))
 
-		ele = {}
+		ele = dict()
 
 		# Get id
 		for key in rdf_data.attrib.keys():
@@ -147,10 +198,13 @@ class CIMClass:
 		"""Returns the element that matches ID"""
 		if search_eles is None:
 			search_eles = self._ele
+		elif not isinstance(search_eles, list):
+			search_eles = self.get_elements_by_tag(search_eles)
 
 		ele = [eles for eles in search_eles if eles['id'] == ele_id]
 		if len(ele) == 0:
 			logger.warning('no element found for ID: {}'.format(ele_id))
+			return None
 		elif len(ele) > 1:
 			logger.warning('{} elements found for ID: {}'.format(len(ele), ele_id))
 
@@ -186,7 +240,7 @@ class CIMClass:
 
 	def save_opendss(self):
 		# Clear all saved DSS elements
-		self._dss_ele = []
+		self._dss_ele = dict()
 
 		# Remove all OpenDSS files in output
 		for file_name in os.listdir(self._output_dir):
@@ -219,12 +273,12 @@ class CIMClass:
 
 		# Start the network parsing process
 		cn_list = [ss_cn]
-		cn_old = []
+		cn_old = list()
 
-		terminal_list = []
-		terminal_old = []
+		terminal_list = list()
+		terminal_old = list()
 
-		saved_equipment = []
+		saved_equipment = list()
 
 		logger.debug(10*'-' + ' PARSING ' + 10*'-')
 
@@ -258,19 +312,237 @@ class CIMClass:
 		f = open(os.path.join(self._output_dir, 'master.dss'), 'a')
 		f.close()
 
+	def _get_terminals(self, equipment):
+		terminals = self.get_elements_by_resource(equipment['id'], 'Terminal')
+		cn_ids = [t['ConnectivityNode'] for t in terminals]
+		sequence = [int(t['sequenceNumber']) - 1 for t in terminals]
+		terminals = [terminals[i] for i in sequence]
+		cn = [self.get_element_by_id(ele_id) for ele_id in cn_ids]
+		return cn, terminals
+
+	def _save_new_dss_transformer(self, dss):
+		if 'transformer' not in self._dss_ele.keys():
+			self._dss_ele['transformer'] = list()
+		self._dss_ele['transformer'].append(dss['id'])
+
+		dss['txfrmr_name'] = 'txfrmr_' + str(len(self._dss_ele['transformer']))
+		with open(os.path.join(self._output_dir, 'transformers.dss'), 'a') as f:
+			f.write('new Transformer.{} windings={} xhl={} basefreq={} sub={}\n'.format(
+				dss['txfrmr_name'], dss['txfrmr_windings'], dss['txfrmr_xhl'], dss['txfrmr_basefreq'], dss['txfrmr_sub']
+			))
+			for i in range(dss['txfrmr_windings']):
+				dss['wdg_bus'][i] += '.1.2.3'
+				if dss['wdg_conn'][i] in ['w', 'wye']:
+					dss['wdg_bus'][i] += '.0'
+				f.write(' ~ Wdg={} Bus={} Conn={} Kv={} Kva={} rneut={} xneut={}\n'.format(
+					i, dss['wdg_bus'][i], dss['wdg_conn'][i], dss['wdg_kv'][i], dss['wdg_kva'][i], dss['wdg_rneut'][i],
+					dss['wdg_xneut'][i]
+				))
+			f.close()
+
+	def _save_new_dss_line(self, dss):
+		# Before saving the line, save the linecode
+		if 'linecode' not in self._dss_ele.keys():
+			self._dss_ele['linecode'] = list()
+
+		# Fix the linecode so that OpenDSS can use it
+		dss['line_linecode_fixed'] = re.sub('[^0-9a-zA-Z]+', '_', dss['line_linecode'])
+		if dss['line_linecode_fixed'] not in self._dss_ele['linecode']:
+
+			linecode = dict()
+			linecode['name'] = dss['line_linecode_fixed']
+			if 'cable' in self._equipment.keys() and dss['line_linecode'] in self._equipment['cable']:
+				linecode_info = self._equipment['cable'][dss['line_linecode']]
+				linecode['r1'] = linecode_info['PositiveSequenceResistance']
+				linecode['x1'] = linecode_info['PositiveSequenceReactance']
+				linecode['r0'] = linecode_info['ZeroSequenceResistance']
+				linecode['x0'] = linecode_info['ZeroSequenceReactance']
+				linecode['normamps'] = linecode_info['NominalRating']
+				linecode['emergamps'] = linecode_info['FourthRating']
+			elif 'overhead' in self._equipment.keys() and dss['line_linecode'] in self._equipment['overhead']:
+				linecode_info = self._equipment['overhead'][dss['line_linecode']]
+				# linecode['r1'] = linecode_info[]
+				# linecode['x1'] = linecode_info[]
+				# linecode['r0'] = linecode_info[]
+				# linecode['x0'] = linecode_info[]
+				# linecode['normamps'] = linecode_info[]
+				# linecode['emergamps'] = linecode_info[]
+			else:
+				logger.warning('linecode {} not found, so I\'m using default instead'.format(dss['line_linecode']))
+				linecode['r1'] = 0.4
+				linecode['x1'] = 1.4
+				linecode['r0'] = 0.4
+				linecode['x0'] = 1.4
+				linecode['normamps'] = 100
+				linecode['emergamps'] = 120
+
+			linecode['units'] = 'km'
+			linecode['basefreq'] = 50.0
+			linecode['nphases'] = 3
+
+			with open(os.path.join(self._output_dir, 'linecode.dss'), 'a') as f:
+				f.write('New Linecode.{} Nphases={} R1={} X1={} R0={} X0={} Units={} BaseFreq={} Normamps={} Emergamps={}\n'.format(
+					linecode['name'], linecode['nphases'], linecode['r1'], linecode['x1'], linecode['r0'], linecode['x0'],
+					linecode['units'], linecode['basefreq'], linecode['normamps'], linecode['emergamps']
+				))
+				f.close()
+			self._dss_ele['linecode'].append(dss['line_linecode_fixed'])
+
+		# Save the line itself
+		if 'line' not in self._dss_ele.keys():
+			self._dss_ele['line'] = list()
+		self._dss_ele['line'].append(dss['id'])
+
+		dss['line_name'] = 'line_' + str(len(self._dss_ele['line']))
+		with open(os.path.join(self._output_dir, 'lines.dss'), 'a') as f:
+			f.write('new Line.{} bus1={} bus2={} Linecode={} Length={} Phases={} Units={}\n'.format(
+				dss['line_name'], dss['line_bus'][0], dss['line_bus'][1], dss['line_linecode_fixed'],
+				dss['line_length'], dss['line_phases'], dss['line_units']
+			))
+			f.close()
+
+	def _save_new_dss_coordinates(self, buses, coords):
+		if 'buses' not in self._dss_ele.keys():
+			self._dss_ele['buses'] = list()
+		for i in range(len(buses)):
+			if buses[i] in self._dss_ele['buses']:
+				continue
+
+			if len(buses) * 2 != len(coords):
+				logger.error('number of buses and coordinates do not match ({} != {})'.format(len(buses), len(coords) // 2))
+				continue
+			with open(os.path.join(self._output_dir, 'buscoords.dss'), 'a') as f:
+				f.write('{}, {:.10f}, {:.10f}\n'.format(buses[i], coords[i*2], coords[i*2+1]))
+				f.close()
+			self._dss_ele['buses'].append(buses[i])
+
 	def save_opendss_for_equipment(self, equipment):
 		logger.info('saving equipment: {}'.format(equipment['id']))
 
-		def get_terminals(self, equipment):
-			terminals = self.get_elements_by_resource(equipment['id'], 'Terminal')
-			cn_ids = [t['ConnectivityNode'] for t in terminals]
-			sequence = [int(t['sequenceNumber'])-1 for t in terminals]
-			terminals = [terminals[i] for i in sequence]
-			cn = [self.get_element_by_id(ele_id) for ele_id in cn_ids]
-			return cn, terminals
-
-		cn, terminals = get_terminals(self, equipment)
+		cn, terminals = self._get_terminals(equipment)
 
 		# TODO: Convert the equipment to OpenDSS
+		if equipment['tag'] == 'PowerTransformer':
 
+			# Find transformer asset
+			txfrmr_asset = self.get_elements_by_resource(equipment['id'], 'Asset')
+			if len(txfrmr_asset) == 0:
+				logger.error('transformer ({}) has no asset'.format(equipment['id']))
+				return cn, terminals
+			elif len(txfrmr_asset) > 1:
+				logger.warning('{} assets found for transformer ({})'.format(len(txfrmr_asset), equipment['id']))
+			txfrmr_asset = txfrmr_asset[0]
+
+			# Find transformer information
+			txfrmr_info = self.get_element_by_id(txfrmr_asset['AssetInfo'])
+			if txfrmr_info is None:
+				logger.error('cannot find information for transforemer asset {}'.format(txfrmr_asset['AssetInfo']))
+				return cn, terminals
+
+			# Populate dss and save
+			dss = dict()
+			if 'transformer' in self._equipment.keys() and txfrmr_info['name'] in self._equipment['transformer']:
+				txfrmr_info = self._equipment['transformer'][txfrmr_info['name']]
+				dss['wdg_kv'] = [float(txfrmr_info['PrimaryVoltageKVLL']), float(txfrmr_info['SecondaryVoltageKVLL'])]
+				dss['wdg_kva'] = [float(txfrmr_info['NominalRatingKVA']), float(txfrmr_info['NominalRatingKVA'])]
+				dss['wdg_conn'] = ['delta', 'wye']
+				dss['wdg_rneut'] = [float(txfrmr_info['PrimGroundingResistanceOhms']), float(txfrmr_info['SecGroundingResistanceOhms'])]
+				dss['wdg_xneut'] = [float(txfrmr_info['PrimGroundingReactanceOhms']), float(txfrmr_info['SecGroundingReactanceOhms'])]
+				dss['txfrmr_xhl'] = float(txfrmr_info['XR0Ratio'])
+			else:
+				dss['wdg_kv'] = [11.0, 0.4]
+				dss['wdg_kva'] = [500.0, 500.0]
+				dss['wdg_conn'] = ['delta', 'wye']
+				dss['wdg_rneut'] = [0.0, 0.0]
+				dss['wdg_xneut'] = [0.0, 0.0]
+				dss['txfrmr_xhl'] = 8.0
+
+			dss['id'] = equipment['id']
+			dss['wdg_bus'] = [c['id'] for c in cn]
+			dss['txfrmr_windings'] = 2
+			dss['txfrmr_basefreq'] = 50.0
+			dss['txfrmr_sub'] = 'y'
+
+			self._save_new_dss_transformer(dss)
+
+		elif equipment['tag'] in ['BusbarSection', 'Disconnector', 'Fuse', 'LoadBreakSwitch']:
+
+			# Make a dummy line element
+			dss = dict()
+			dss['id'] = equipment['id']
+			dss['line_bus'] = [c['id'] for c in cn]
+			dss['line_length'] = 1e-4
+			dss['line_linecode'] = 'DEFAULT'
+			dss['line_phases'] = 3
+			dss['line_units'] = 'm'
+
+			self._save_new_dss_line(dss)
+			if 'coords' in equipment.keys() and len(equipment['coords']) == 2 * len(dss['line_bus']):
+				self._save_new_dss_coordinates(dss['line_bus'], equipment['coords'])
+		elif equipment['tag'] == 'ACLineSegment':
+
+			# Get line asset
+			line_asset = self.get_elements_by_resource(equipment['id'], 'Asset')
+
+			dss = dict()
+			dss['id'] = equipment['id']
+			dss['line_bus'] = [c['id'] for c in cn]
+			dss['line_phases'] = 3
+			dss['line_units'] = 'm'
+
+			# Check if it's overhead (boken into wires) or underground (single cable)
+			if equipment['PSRType'] == 'PSRType_Overhead':
+				if len(line_asset) != 2 and len(line_asset) != 4:
+					logger.warning('found {} assets overhead wire {}'.format(len(line_asset), equipment['id']))
+					dss['line_linecode'] = 'DEFAULT'
+				else:
+					wire_info = self.get_element_by_id(line_asset[0]['AssetInfo'], 'OverheadWireInfo')
+					dss['line_linecode'] = wire_info['name']
+			elif equipment['PSRType'] == 'PSRType_Underground':
+				if len(line_asset) != 1:
+					logger.warning('found {} assets for underground cable {}'.format(len(line_asset), equipment['id']))
+					dss['line_linecode'] = 'DEFAULT'
+				else:
+					cable_info = self.get_element_by_id(line_asset[0]['AssetInfo'], 'CableInfo')
+					dss['line_linecode'] = cable_info['name']
+			else:
+				logger.warning('unknown PSRType {}'.format(equipment['PSRType']))
+				dss['line_linecode'] = 'DEFAULT'
+
+			# Populate length correctly
+			dss['line_length'] = equipment['length']
+			if 'coords' in equipment.keys():
+				# Break into indicidual lines
+				line_length_orginal = dss['line_length']
+				line_bus_original = dss['line_bus'].copy()
+
+				length_scales = list()
+				length_scales_sum = 0.0
+				c = equipment['coords']
+				for i in range(2, len(c), 2):
+					length_scales.append(math.sqrt(math.pow(c[i]-c[i-2], 2) + math.pow(c[i+1]-c[i-1], 2)))
+					length_scales_sum += length_scales[-1]
+
+				for i in range(len(length_scales)):
+					length_scales[i] /= length_scales_sum
+
+				for i in range(len(length_scales)):
+					if i == 0:
+						dss['line_bus'][0] = line_bus_original[0]
+					else:
+						dss['line_bus'][0] = line_bus_original[0] + '_' + str(i)
+
+					if i == len(length_scales) - 1:
+						dss['line_bus'][1] = line_bus_original[1]
+					else:
+						dss['line_bus'][1] = line_bus_original[0] + '_' + str(i+1)
+
+					dss['line_length'] = float(line_length_orginal) * length_scales[i]
+					self._save_new_dss_line(dss)
+					self._save_new_dss_coordinates(dss['line_bus'], c[i*2:i*2+4])
+			else:
+				self._save_new_dss_line(dss)
+
+		else:
+			logger.warning('element {} not yet implemented'.format(equipment['tag']))
 		return cn, terminals
