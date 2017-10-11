d74224b072577e2e775f24ed8ce9c4d714abe96d
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-10-11 12:06:51 +0200

    Changed trial so a single time stamp is assessed

--- source/matlab/main.m
+++ source/matlab/main.m
@@ -10,28 +10,34 @@ dss_data_dir = dir('../Daily_1min_100profiles/*.txt');
 [dss, data] = setup_dss(dss_master_path, '318', dss_data_dir);
 
 % Remove last three elements in data
-data = data(:, 1:dss.get_load_count);
-data(:, end-2:end) = 0;
+data = data(1, 1:dss.get_load_count);
+data(1, end-2:end) = 0;
 
 [actual_load, actual_voltages] = solve_dss(dss, data);
 
-figure(1);
-yyaxis left
-plot((1:1440)/60, actual_load)
-
-yyaxis right
-plot((1:1440)/60, mean(actual_voltages, 2));
-ylabel('power (kVA)');
-xlabel('time (h)');
-hold on
-plot((1:1440)/60, max(actual_voltages, [], 2));
-plot((1:1440)/60, min(actual_voltages, [], 2));
-hold off
-ylabel('voltage (V)');
-drawnow
+% figure(1);
+% yyaxis left
+% plot((1:size(data, 1))/60, actual_load)
+% 
+% yyaxis right
+% plot((1:size(data, 1))/60, mean(actual_voltages, 2));
+% ylabel('power (kVA)');
+% xlabel('time (h)');
+% hold on
+% plot((1:size(data, 1))/60, max(actual_voltages, [], 2));
+% plot((1:size(data, 1))/60, min(actual_voltages, [], 2));
+% hold off
+% ylabel('voltage (V)');
+% drawnow
 
 %% Generate random loads that has the same power profile n times
-profile_reps = 2; % Define the number of profile repetitions
+profile_reps = 10; % Define the number of profile repetitions
 [ rand_load ] = match_random_load( profile_reps, actual_load, dss, true );
 
-save 
+save tmp
+
+%% Match random loads to voltages
+
+idx = dss.get_load_count() - [2 1 0]; % Voltages that should be matched
+[ rand_load_v ] = match_random_voltage( rand_load, actual_voltages, idx, dss );
+
