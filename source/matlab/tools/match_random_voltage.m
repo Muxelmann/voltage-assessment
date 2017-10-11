baa3ce93c473a9a7f13cd0605f9a2ca02b254479
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-10-11 12:06:21 +0200

    Wrote voltage adjustment code

--- source/matlab/tools/match_random_voltage.m
+++ source/matlab/tools/match_random_voltage.m
@@ -1,35 +1,6 @@
-close all
-clear
-addpath('tools/');
-
-load tmp
-
-% Run basic simulation
-dss_master_path = dir('../LVTestCase/Master.dss');
-dss_data_dir = dir('../Daily_1min_100profiles/*.txt');
-
-[dss, ~] = setup_dss(dss_master_path, '318', dss_data_dir);
-
-%% Assess the voltages
-
-rand_load_v = rand_load(end);
-
-[~, rand_voltages] = solve_dss(dss, rand_load_v{end});
-
-rand_voltages_error = rand_voltages - repmat(actual_voltages, profile_reps, 1);
-rand_voltages_error = rand_voltages_error(:, end-2:end);
-
-plot(rand_voltages_error);
-
-% %% Test adjustment
-% 
-% adj_data = repmat(data(1, :), 100, 1);
-% adj_proportion = ones(100, 6) * 0.5;
-% adj_proportion(:, 1) = linspace(0, 1, 100);
-% adj_proportion(:, 4) = 1 - adj_proportion(:, 1);
-% adj_data_2 = adjust_load_shapes(adj_proportion, adj_data, dss); 
-
-%%
+function [ rand_load_next ] = match_random_voltage( rand_load, actual_voltages, idx, dss )
+%MATCH_RANDOM_VOLTAGE Summary of this function goes here
+%   Detailed explanation goes here
 
 load_location = dss.get_load_location(true);
 
@@ -49,14 +20,14 @@ fmincon_opt.OptimalityTolerance = 1e-4;
 fmincon_opt.Display = 'none';
 fmincon_opt.FiniteDifferenceStepSize = 1e-3;
 
-rand_load_next = rand_load_v{end};
+rand_load_next = rand_load{end};
 adjust_tol = 1e5;
 for t = 1:size(rand_load_next, 1)
     fprintf('starting: %5d', t);
-    rand_load_t = rand_load_v{end}(t, :);
+    rand_load_t = rand_load_next(t, :);
     t_actual = mod(t-1, size(actual_voltages, 1)) + 1;
     v_actual = actual_voltages(t_actual, :);
-    [rand_load_adj, ~, exitflag] = fmincon(@(x) cost_voltage_offset(x, rand_load_t, dss, v_actual, dss.get_load_count() - [2 1 0]), ...
+    [rand_load_adj, ~, exitflag] = fmincon(@(x) cost_voltage_offset(x, rand_load_t, dss, v_actual, idx), ...
         x0, A, b, Aeq, beq, lb, ub, [], fmincon_opt);
     
     % Check that the solver exited correctly
@@ -68,13 +39,5 @@ for t = 1:size(rand_load_next, 1)
     fprintf(' -> DONE\n');
 end
 
-% Halt for now
-return
-%%
-
-diff = rand_load_next - rand_load_v{end};
-
-imagesc(diff)
-
-
+end
 
