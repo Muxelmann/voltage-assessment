2d5add51d684a7dccb1fadbd4ae1b8e573c17393
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-10-11 13:49:13 +0200

    It works (sort of)
    Because the voltage matching can scale down to 0, some results do not converge...

--- source/matlab/main.m
+++ source/matlab/main.m
@@ -10,15 +10,22 @@ dss_data_dir = dir('../Daily_1min_100profiles/*.txt');
 [dss, data] = setup_dss(dss_master_path, '318', dss_data_dir);
 
 % Remove last three elements in data
-data = data(1, 1:dss.get_load_count);
+data = data(:, 1:dss.get_load_count);
 data(1, end-2:end) = 0;
+
+[~, max_idx] = max(sum(data, 2));
+data = data(max_idx, :);
+clear max_idx;
 
 [actual_load, actual_voltages] = solve_dss(dss, data);
 
 %% Generate random loads that has the same power profile n times
 
-profile_reps = 10; % Define the number of profile repetitions
-rand_load = match_random_load(profile_reps, actual_load, dss, true);
+profile_reps = 2; % Define the number of profile repetitions
+[rand_load, i_remain] = match_random_load(profile_reps, actual_load, dss, true);
+
+assert(i_remain > 0)
+clear i_remain
 
 save tmp_p
 
@@ -32,7 +39,40 @@ save tmp_v
 %% Test how well the solution converged
 
 test_pass = convergence_test(rand_load_final, actual_load, actual_voltages, idx, dss);
-assert(test_pass, 'Solution not converged for both P and V');
-clear test_pass
 
+
+
+%% Try and fix missing convergence
+
+% I found that when scaling down loads within a load zone, they do actually
+% reach zero at some stage and cannot recover. This leads to a discrepancy
+% in the demand profile and causes the solution not to converge (maybe).
+% 
+% Consider using two scaling mechanisms for the random load profiles:
+% 1. proportional:  where the loads are scaled up and down based on their
+%                   relative contribution to the overall profile
+% 2. uniformly:     where the loads are scaled equally regardless of their
+%                   relative contribution to the overall profile
+%
+% By implementing the 2. scaling, zero loads (apart from ESMU) can be
+% re-included into the solving mechanism.
+
+while any(test_pass) == 0
+    if all(test_pass == 0)
+        disp('Everything needs to be re-evaluated');
+    elseif any(test_pass == 0)
+        disp('Some simulations need to be re-evaluated');
+    else
+        disp('Nothing needs to be re-evaluated');
+    end
+    
+    [rand_load, i_remain] = match_random_load({rand_load_final}, actual_load, dss, true);
+
+    assert(i_remain > 0)
+    clear i_remain
+
+    rand_load_final = match_random_voltage(rand_load, actual_voltages, idx, dss);
+    
+    test_pass = convergence_test(rand_load_final, actual_load, actual_voltages, idx, dss);
+end
 
--- source/matlab/tools/match_random_voltage.m
+++ source/matlab/tools/match_random_voltage.m
@@ -1,6 +1,10 @@
-function [ rand_load_next ] = match_random_voltage( rand_load, actual_voltages, idx, dss )
+function [ rand_load_next ] = match_random_voltage( rand_load, actual_voltages, idx, dss, do_plot )
 %MATCH_RANDOM_VOLTAGE Summary of this function goes here
 %   Detailed explanation goes here
+
+if exist('do_plot', 'var') == 0
+    do_plot = false;
+end
 
 load_location = dss.get_load_location(true);
 
@@ -15,10 +19,15 @@ ub = ones(1, size(load_location, 2) * size(load_location, 3));
 x0 = ones(1, size(load_location, 2) * size(load_location, 3)) * 0.5;
 
 fmincon_opt = optimoptions(@fmincon);
-fmincon_opt.PlotFcns = {@optimplotx, @optimplotfval};
+% fmincon_opt.Algorithm = 'sqp';
+if do_plot
+    fmincon_opt.PlotFcns = {@optimplotx, @optimplotfval};
+end
 fmincon_opt.OptimalityTolerance = 1e-4;
 fmincon_opt.Display = 'none';
 fmincon_opt.FiniteDifferenceStepSize = 1e-3;
+fmincon_opt.MaxIterations = 2.5e3;
+fmincon_opt.MaxFunctionEvaluations = 7.5e3;
 
 rand_load_next = rand_load{end};
 adjust_tol = 1e5;
@@ -27,16 +36,22 @@ for t = 1:size(rand_load_next, 1)
     rand_load_t = rand_load_next(t, :);
     t_actual = mod(t-1, size(actual_voltages, 1)) + 1;
     v_actual = actual_voltages(t_actual, :);
-    [rand_load_adj, ~, exitflag] = fmincon(@(x) cost_voltage_offset(x, rand_load_t, dss, v_actual, idx), ...
+    tic;
+    [rand_load_adj, fval, exitflag] = fmincon(@(x) cost_voltage_offset(x, rand_load_t, dss, v_actual, idx), ...
         x0, A, b, Aeq, beq, lb, ub, [], fmincon_opt);
-    
+    t_elapsed = toc;
     % Check that the solver exited correctly
     assert(exitflag > 0);
     % Make sure all loads are balanced
     assert(all(round(sum(reshape(rand_load_adj, size(load_location, 2), size(load_location, 3)), 2) * adjust_tol) == adjust_tol));
     
-    rand_load_next(t, :) = adjust_load_shapes(rand_load_adj, rand_load_t, dss);
-    fprintf(' -> DONE\n');
+    % Calculate adjusted load
+    rand_load_t_next = adjust_load_shapes(rand_load_adj, rand_load_t, dss);
+    % make sure it didn't change too much from original
+    assert(round((sum(rand_load_t_next) - sum(rand_load_t)) * 1e9) == 0);
+    
+    rand_load_next(t, :) = rand_load_t_next;
+    fprintf(' -> DONE (%.2fs with %f error)\n', t_elapsed, fval);
 end
 
 end
