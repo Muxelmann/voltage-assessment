17cc0434a65c5252fe3f05e19645f62a15936d5e
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-11-29 16:47:35 +0100

    Added a callback so that progress is known even when no plots are made

--- source/matlab/tools/match_random_load_and_voltage_at_once.m
+++ source/matlab/tools/match_random_load_and_voltage_at_once.m
@@ -1,4 +1,4 @@
-function [ rand_profiles ] = match_random_load_and_voltage( ...
+function [ rand_profiles ] = match_random_load_and_voltage_at_once( ...
 profile_reps, actual_load, actual_voltages, idx, dss, ignore_idx, do_plot )
 %MATCH_RANDOM_VOLTAGE Matches both voltages and 
 
@@ -14,7 +14,6 @@ end
 rand_profiles = arrayfun(@(x) make_random_profile(actual_load, dss, ignore_idx), 1:profile_reps, 'uni', 0);
 
 load_location = dss.get_load_location(true);
-load_count = size(load_location, 1);
 phase_count = size(load_location, 2);
 zone_count = size(load_location, 3);
 
@@ -38,11 +37,12 @@ for n = 1:length(rand_profiles)
     lb = 1e-4 * ones(size(x0));
     ub = 1e+1 * ones(size(x0));
     
-    
     fmincon_opt = optimoptions(@fmincon);
     % fmincon_opt.Algorithm = 'sqp';
     if do_plot
         fmincon_opt.PlotFcns = {@optimplotx, @optimplotfval};
+    else
+        fmincon_opt.OutputFcn = @progress_callback;
     end
     fmincon_opt.OptimalityTolerance = 1e-4;
     fmincon_opt.Display = 'none';
@@ -52,6 +52,7 @@ for n = 1:length(rand_profiles)
     
     fprintf('starting: %5d of %5d\n', n, length(rand_profiles));
     
+    print_string = '';
     t_start = now * 60*60*24;
     [rand_load_adj, fval, exitflag] = fmincon(@(x) cost_vp_offset(x, rand_profile, dss, actual_load, actual_voltages, idx), ...
         x0, A, b, Aeq, beq, lb, ub, [], fmincon_opt);
@@ -77,5 +78,14 @@ for n = 1:length(rand_profiles)
     save('tmp_all.mat', 'rand_profiles', 'rand_profiles_pass', ...
         'actual_load', 'actual_voltages', 'idx', 'ignore_idx');
 end
+
+    function stop = progress_callback(~, optimValues, ~)
+        t_elapsed = now * 60*60*24 - t_start;
+        fprintf(repmat('\b', 1, length(print_string)));
+        print_string = sprintf('-> iteration: %d, cost: %.2f, time elapsed: %.2fs\n', ...
+            optimValues.iteration, optimValues.fval, t_elapsed);
+        fprintf(print_string)
+        stop = false;
+    end
 
 end
