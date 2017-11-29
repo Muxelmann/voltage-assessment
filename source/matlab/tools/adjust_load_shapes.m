be6cfbf55b69673a7271761489fa52d5048ea358
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-11-29 12:31:15 +0100

    Loadshape adjustment is changed
    Now 1 implies no change to original load, <1 is lowering of group in phase and >1 is increase of group in phase

--- source/matlab/tools/adjust_load_shapes.m
+++ source/matlab/tools/adjust_load_shapes.m
@@ -1,34 +1,34 @@
-function [ load_new ] = adjust_load_shapes( load_proportion, load, dss )
+function [ loads_new ] = adjust_load_shapes( loads_scaling, loads_original, dss )
 %ADJUST_LOAD_SHAPES Assigns load proportion to upper and lower energy zone
 
-if size(load_proportion, 1) > 1 && size(load, 1) > 1
-    load_new = arrayfun(@(x) adjust_load_shapes(load_proportion(x, :), load(x, :), dss), 1:size(load, 1), 'uni', 0);
-    load_new = cell2mat(load_new.');
+if size(loads_scaling, 1) > 1 && size(loads_original, 1) > 1
+    loads_new = arrayfun(@(x) adjust_load_shapes(loads_scaling(x, :), loads_original(x, :), dss), 1:size(loads_original, 1), 'uni', 0);
+    loads_new = cell2mat(loads_new.');
     return
 end
 
-assert(size(load_proportion, 1) == 1 && size(load, 1) == 1);
+assert(size(loads_scaling, 1) == 1 && size(loads_original, 1) == 1);
 
 % Get the load locations
 load_location = dss.get_load_location();
+load_count = size(load_location, 1);
+phase_count = size(load_location, 2);
+zone_count = size(load_location, 3);
 
-% Map load proportion
-load_proportion = reshape(load_proportion, 1, size(load_location, 2), size(load_location, 3));
-load_proportion = repmat(load_proportion, size(load_location, 1), 1, 1);
+% Map load scaling to each load
+loads_scaling = reshape(loads_scaling, 1, phase_count, zone_count);
+loads_scaling = repmat(loads_scaling, load_count, 1, 1);
 
 % Find the fraction of load that is consumed by each load within its energy
 % zone and phase
-load_frac = repmat(load(:), 1, size(load_location, 2), size(load_location, 3));
+load_frac = repmat(loads_original(:), 1, phase_count, zone_count);
 load_frac(load_location == 0) = 0;
-load_total = repmat(sum(load_frac, 1), size(load_location, 1), 1, 1);
+load_total = repmat(sum(load_frac, 1), load_count, 1, 1);
 load_frac = load_frac ./ load_total;
 
-% Find the total load per phase
-load_total = repmat(sum(load_total, 3), 1, 1, size(load_location, 3));
-
 % Multiply with load proportion to get new but correctly scaled
-load_new = load_frac .* load_total .* load_proportion;
-load_new = sum(sum(load_new, 3), 2).';
+loads_new = load_frac .* load_total .* loads_scaling;
+loads_new = sum(sum(loads_new, 3), 2).';
 
 end
 
