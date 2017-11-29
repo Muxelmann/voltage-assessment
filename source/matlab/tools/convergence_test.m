330890b765546932b81a7b9a074124e3a14c417e
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-11-29 12:31:46 +0100

    convergence test is laxed
    Now <1V and <1kW means solution converged

--- source/matlab/tools/convergence_test.m
+++ source/matlab/tools/convergence_test.m
@@ -1,13 +1,12 @@
-function [ test_pass ] = convergence_test( data, actual_load, actual_voltages, idx, dss )
-%TEST_CONVERGENCE Summary of this function goes here
-%   Detailed explanation goes here
+function [ test_pass ] = convergence_test( random_profiles, actual_load, actual_voltages, idx, dss )
+%TEST_CONVERGENCE Checks if the results from `random_profiles` match
 
-[ss_power, load_voltages] = solve_dss(dss, data);
+[ss_power, load_voltages] = solve_dss(dss, random_profiles);
 
 load_error = ss_power - actual_load;
 voltage_error = load_voltages(:, idx) - actual_voltages(:, idx);
 
-test_pass = all(abs(voltage_error) < 0.1, 2) & all(abs(load_error) < 1e-3, 2);
+test_pass = all(abs(voltage_error) < 1, 2) & all(abs(load_error) < 1, 2);
 
 end
 
