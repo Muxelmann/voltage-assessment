beffa0ae65b148d1c00f2ec577296abe05497655
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-11-29 12:42:02 +0100

    Renamed variables

--- source/matlab/tools/cost_voltage_offset.m
+++ source/matlab/tools/cost_voltage_offset.m
@@ -1,9 +1,9 @@
-function [ cost ] = cost_voltage_offset(load_proportion, loads, dss, voltages, idx )
+function [ cost ] = cost_voltage_offset(loads_scaling, loads_original, dss, voltages, idx )
 %COST_VOLTAGE_OFFSET Summary of this function goes here
 %   Detailed explanation goes here
 
-adj_load = adjust_load_shapes(load_proportion, loads, dss);
-[~, v_sim] = solve_dss(dss, adj_load);
+loads_new = adjust_load_shapes(loads_scaling, loads_original, dss);
+[~, v_sim] = solve_dss(dss, loads_new);
 
 cost = sum(abs(v_sim(:, idx) - voltages(:, idx)), 2);
 
